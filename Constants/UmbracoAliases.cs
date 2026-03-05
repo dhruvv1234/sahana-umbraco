7278d9f7332083ca18bfaff5169ebc0c5cc9aa5e
dhruv trivedi <dhruvtrivedi2002@gmail.com>
2026-03-05 16:57:14 +0530

    umbraco setup

---
+++ Constants/UmbracoAliases.cs
@@ -0,0 +1,37 @@
+﻿namespace sahanaweb.Constants
+{
+    public class UmbracoAliases
+    {
+        public static class HeaderAliases
+        {
+            public static class Properties
+            {
+                public const string SiteNames = "siteNames";
+                public const string WhiteLogo = "whiteLogo";
+                public const string Logo = "logo";
+                public const string Links = "links";
+                public const string UsefulLinkTitle = "usefulLinkTitle";
+                public const string CopyrightText = "copyrightText";
+                public const string SiteUrl = "siteUrl";
+                public const string Icon = "icon";
+                public const string SubHeading = "subHeading";
+                public const string SubmitButton = "submitButton";
+                public const string FormSelection = "formSelection";
+                public const string NewslettterSection = "newslettterSection";
+                public const string Search = "search";
+                public const string Account = "account";
+                public const string SocialMediaLinks = "socialMediaLinks";
+            }
+
+            public static class Navigation
+            {
+                public const string ShowInNavigation = "showInNavigation";
+                public const string isClickable = "isClickable";
+                public const string NavigationTitle = "navigationTitle";
+            }
+
+
+
+        }
+    }
+}
---
+++ Controller/HeaderController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+using static sahanaweb.Constants.UmbracoAliases;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Web.Common.PublishedModels;
+namespace sahanaweb.Controller
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class HeaderController : ControllerBase
+    {
+        private readonly IUmbracoContextFactory _umbraco;
+        public HeaderController(IUmbracoContextFactory umbraco)
+        {
+            _umbraco = umbraco;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetHeaderData()
+        {
+            using var refCtx = _umbraco.EnsureUmbracoContext();
+            var ctx = refCtx.UmbracoContext;
+
+            var home = ctx.Content.GetAtRoot()
+                .FirstOrDefault(x => x.ContentType.Alias == "home");
+
+            if (home == null)
+                return NotFound("Home node not found.");
+
+            var props = home.Value<IPublishedElement>("header");
+
+            var headerData = new
+            {
+                siteNames = home?.Value<string>(HeaderAliases.Properties.SiteNames),
+                whiteLogo = home?.Value<IPublishedContent>(HeaderAliases.Properties.WhiteLogo)?.Url(),
+                logo = home?.Value<IPublishedContent>(HeaderAliases.Properties.Logo)?.Url(),
+                links = home?.Value<IEnumerable<Link>>(HeaderAliases.Properties.Links),
+                usefulLinkTitle = home?.Value<string>(HeaderAliases.Properties.UsefulLinkTitle),
+                copyrightText = home?.Value<string>(HeaderAliases.Properties.CopyrightText),
+                siteUrl = home?.Value<Link>(HeaderAliases.Properties.SiteUrl),
+                icon = home?.Value<IEnumerable<IPublishedContent>>(HeaderAliases.Properties.Icon),
+                subHeading = home?.Value<string>(HeaderAliases.Properties.SubHeading),
+                submitButton = home?.Value<string>(HeaderAliases.Properties.SubmitButton),
+                newslettterSection = home?.Value<string>(HeaderAliases.Properties.NewslettterSection),
+                search = home?.Value<string>(HeaderAliases.Properties.Search),
+                account = home?.Value<string>(HeaderAliases.Properties.Account),
+                socialMediaLinks = home?.Value<IEnumerable<Link>>(HeaderAliases.Properties.SocialMediaLinks)
+            };
+
+            var navItems = BuildNavigation(home.Children());
+
+            return Ok(new { header = headerData, navMenu = navItems });
+        }
+
+        private IEnumerable<object> BuildNavigation(IEnumerable<IPublishedContent> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                bool show = node.Value<bool>(HeaderAliases.Navigation.ShowInNavigation);
+                if (!show) continue;
+
+                bool clickable = node.Value<bool>(HeaderAliases.Navigation.isClickable);
+
+                yield return new
+                {
+                    id = node.Id,
+                    title = node.Value<string>(HeaderAliases.Navigation.NavigationTitle) ?? node.Name,
+                    url = clickable ? node.Url() : "#",
+                    clickable,
+                    showInNavigation = true,
+                    children = BuildNavigation(node.Children())
+                };
+            }
+        }
+    }
+}
